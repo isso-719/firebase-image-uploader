cfab8478df7c48cc78849d21b4b4a458f964d8ec
isso-719 <i@kazukiisogai.net>
2022-02-13 00:35:10 +0900

    add comments

--- app.rb
+++ app.rb
@@ -19,8 +19,10 @@ end
 post '/create' do
     if params[:file]
 
+        # storage を取得
         storage = Google::Cloud::Storage.new(credentials: "./credentials.json")
 
+        # bucket を取得
         bucket = storage.bucket ENV['FIREBASE_STORAGE_BUCKET_ID']
 
         # 一時ファイルを作成
@@ -39,9 +41,10 @@ post '/create' do
         # tempfile をストレージに保存
         file = bucket.create_file tempfile, "img/#{tempfile_name}", metadata: { firebaseStorageDownloadTokens: token }
 
-        # 画像の URL を保存する変数
+        # 画像のパブリック URL を保存する変数
         img_url = "https://firebasestorage.googleapis.com/v0/b/#{ENV['FIREBASE_STORAGE_BUCKET_ID']}/o/img%2F#{tempfile_name}?alt=media&token=#{token}"
 
+        # 画像のパブリック URL を表示する
         puts img_url
 
     end
